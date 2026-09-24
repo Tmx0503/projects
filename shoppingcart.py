ab5953731158fd1e7e065b563d7d8c62ab3d6383
Tmx0503 <abrahammark0104@gmail.com>
2026-09-24 21:03:15 +0200

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -2,6 +2,11 @@
 item = input("what item would you like to buy")
 price = float(input("what is the price"))
 quantity = int(input("how much do you want"))
+# 31:35
+
+total = price * quantity
+print(f"you have bought {quantity} x {item}/s")
+print(f"your total is:") 
 
 
 
