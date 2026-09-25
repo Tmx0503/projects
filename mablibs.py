df0279e62a02debe556f04d4aed592977678b27a
Tmx0503 <abrahammark0104@gmail.com>
2026-09-25 17:32:18 +0200

    Create mablibs.py

---
+++ mablibs.py
@@ -0,0 +1,16 @@
+
+adjective1 = input("enter ..")
+adjective2 = input("enter..")
+adjective3 = input("enter")
+noun1 = input("enter")
+verb1 = input("5")
+
+print(f"today i went to {adjective1} zoo")
+print(f"in an exhibit i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"i was {adjective3}")
+
+
+
+
+
