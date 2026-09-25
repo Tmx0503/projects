776b666874b067405e7f8dd59932be4bf6960ac5
Tmx0503 <abrahammark0104@gmail.com>
2026-09-25 17:51:05 +0200

    day 2

---
+++ matkek.py
@@ -0,0 +1,11 @@
+
+
+
+x = 3.14
+y = -4
+z = 5
+# result = round(x)   ez kerekíti a számot
+# result = abs(y) abszolútérték
+# result pow(y, z) hatványozás 
+# max(x, y, z) mi a maxium ezek az értékek között ugyanígy van min() is
+
