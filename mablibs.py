3fdcfd04685c35ab1b920191eb605638b411e04e
Tmx0503 <abrahammark0104@gmail.com>
2026-09-25 17:51:53 +0200

    day2

--- mablibs.py
+++ mablibs.py
@@ -3,7 +3,7 @@ adjective1 = input("enter ..")
 adjective2 = input("enter..")
 adjective3 = input("enter")
 noun1 = input("enter")
-verb1 = input("5")
+verb1 = input("59")
 
 print(f"today i went to {adjective1} zoo")
 print(f"in an exhibit i saw a {noun1}")
